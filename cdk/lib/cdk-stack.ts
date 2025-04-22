e5410e8b81a45c00801d3a90edd2460c95b7ad8a
Sunil Darna <darnasunil@gmail.com>
2025-04-22 06:48:21 +0000

    feat(T4): implement Cognito User Pool

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,18 +5,196 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class BlockPulseStack extends cdk.Stack {
+  // Public properties to expose resources to other parts of the application
+  public readonly userPool: cognito.UserPool;
+  public readonly userPoolClient: cognito.UserPoolClient;
+  public readonly identityPool: cognito.CfnIdentityPool;
+  public readonly authenticatedRole: iam.Role;
+  public readonly unauthenticatedRole: iam.Role;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    // Resources will be added in subsequent tasks (T4-T8)
-    
+    // Environment-specific configuration
+    const isProd = id.includes('Prod');
+    const envName = isProd ? 'prod' : 'dev';
+    const domainPrefix = isProd ? 'blockpulse-prod' : 'blockpulse-dev';
+
+    // T4: Cognito User Pool
+    this.userPool = new cognito.UserPool(this, 'BlockPulseUserPool', {
+      userPoolName: `BlockPulse-${envName}-UserPool`,
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+        username: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+        familyName: {
+          required: true,
+          mutable: true,
+        },
+        email: {
+          required: true,
+          mutable: false,
+        },
+      },
+      customAttributes: {
+        createdAt: new cognito.DateTimeAttribute(),
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+        tempPasswordValidity: cdk.Duration.days(7),
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: isProd ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Add domain to the user pool
+    const userPoolDomain = this.userPool.addDomain('BlockPulseUserPoolDomain', {
+      cognitoDomain: {
+        domainPrefix: domainPrefix,
+      },
+    });
+
+    // Create User Pool Client
+    this.userPoolClient = this.userPool.addClient('BlockPulseUserPoolClient', {
+      userPoolClientName: `BlockPulse-${envName}-client`,
+      generateSecret: false,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+        adminUserPassword: true,
+      },
+      preventUserExistenceErrors: true,
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+          implicitCodeGrant: true,
+        },
+        scopes: [
+          cognito.OAuthScope.EMAIL,
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.PROFILE,
+        ],
+        callbackUrls: [
+          isProd ? 'https://blockpulse.anviinnovate.com/callback' : 'http://localhost:3000/callback',
+        ],
+        logoutUrls: [
+          isProd ? 'https://blockpulse.anviinnovate.com/' : 'http://localhost:3000/',
+        ],
+      },
+      supportedIdentityProviders: [
+        cognito.UserPoolClientIdentityProvider.COGNITO,
+      ],
+    });
+
+    // Create Identity Pool
+    this.identityPool = new cognito.CfnIdentityPool(this, 'BlockPulseIdentityPool', {
+      identityPoolName: `BlockPulse${envName}IdentityPool`,
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: this.userPoolClient.userPoolClientId,
+          providerName: this.userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    // Create IAM roles for authenticated and unauthenticated users
+    this.authenticatedRole = new iam.Role(this, 'BlockPulseAuthenticatedRole', {
+      assumedBy: new iam.FederatedPrincipal(
+        'cognito-identity.amazonaws.com',
+        {
+          StringEquals: {
+            'cognito-identity.amazonaws.com:aud': this.identityPool.ref,
+          },
+          'ForAnyValue:StringLike': {
+            'cognito-identity.amazonaws.com:amr': 'authenticated',
+          },
+        },
+        'sts:AssumeRoleWithWebIdentity'
+      ),
+      description: 'Role for authenticated BlockPulse users',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+      ],
+    });
+
+    this.unauthenticatedRole = new iam.Role(this, 'BlockPulseUnauthenticatedRole', {
+      assumedBy: new iam.FederatedPrincipal(
+        'cognito-identity.amazonaws.com',
+        {
+          StringEquals: {
+            'cognito-identity.amazonaws.com:aud': this.identityPool.ref,
+          },
+          'ForAnyValue:StringLike': {
+            'cognito-identity.amazonaws.com:amr': 'unauthenticated',
+          },
+        },
+        'sts:AssumeRoleWithWebIdentity'
+      ),
+      description: 'Role for unauthenticated BlockPulse users',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+      ],
+    });
+
+    // Attach roles to Identity Pool
+    new cognito.CfnIdentityPoolRoleAttachment(this, 'BlockPulseIdentityPoolRoleAttachment', {
+      identityPoolId: this.identityPool.ref,
+      roles: {
+        authenticated: this.authenticatedRole.roleArn,
+        unauthenticated: this.unauthenticatedRole.roleArn,
+      },
+    });
+
     // Output the stack name for reference
     new cdk.CfnOutput(this, 'StackName', {
       value: this.stackName,
       description: 'The name of the BlockPulse stack',
+    });
+
+    // Output Cognito User Pool ID
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: this.userPool.userPoolId,
+      description: 'The ID of the Cognito User Pool',
+      exportName: `${this.stackName}-UserPoolId`,
+    });
+
+    // Output Cognito User Pool Client ID
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId,
+      description: 'The ID of the Cognito User Pool Client',
+      exportName: `${this.stackName}-UserPoolClientId`,
+    });
+
+    // Output Cognito Identity Pool ID
+    new cdk.CfnOutput(this, 'IdentityPoolId', {
+      value: this.identityPool.ref,
+      description: 'The ID of the Cognito Identity Pool',
+      exportName: `${this.stackName}-IdentityPoolId`,
+    });
+
+    // Output Cognito User Pool Domain
+    new cdk.CfnOutput(this, 'UserPoolDomain', {
+      value: `${domainPrefix}.auth.${this.region}.amazoncognito.com`,
+      description: 'The domain of the Cognito User Pool',
+      exportName: `${this.stackName}-UserPoolDomain`,
     });
   }
 }
